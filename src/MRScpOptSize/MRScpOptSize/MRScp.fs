5a9cfaaebbb6182c1288f000aec8fc74ebd8d3c6
Dimitur Krustev <dkrustev@users.noreply.github.com>
2020-02-02 21:32:19 +0200

    finish initial version of multi-result scp; test on a trivial example

--- src/MRScpOptSize/MRScpOptSize/MRScp.fs
+++ src/MRScpOptSize/MRScpOptSize/MRScp.fs
@@ -5,46 +5,55 @@ open Exp
 open ConfGraphs
 open MultiDrive
 
+type MConf = MultiDriveStepResult * Exp
+
 type GraphSet =
     | GSNone
-    | GSFold of int * list<VarName * VarName>
-    | GSBuild of MultiDriveStepResult * list<list<GraphSet>>
+    | GSFold of MConf * int * list<VarName * VarName>
+    | GSBuild of MConf * list<list<GraphSet>>
 
-let private buildGraph (mdsr: MultiDriveStepResult) (gs: list<ConfGraph>) : ConfGraph =
-    match mdsr with
-    | MDSRLeaf e -> CGLeaf e
-    | MDSRCon(c, es) -> 
-        assert (List.length es = List.length gs)
-        CGCon(c, gs)
-    | MDSRUnfold e -> 
-        assert (List.length gs = 1)
-        CGUnfold(List.head gs)
-    | MDSRCases(x, alts) ->
-        assert (List.length alts = List.length gs)
-        CGCases(x, List.zip (List.map fst alts) gs)
-    | MDSRLet(binds, _e) ->
-        assert (List.length binds + 1 = List.length gs)
-        let (g::gs) = gs
-        CGLet(List.zip (List.map fst binds) gs, g)
+let private buildGraph (e: Exp) (cgs: list<MConf * ConfGraph>) : ConfGraph =
+    let (confs, gs) = List.unzip cgs
+    let mdsrs = List.map fst confs
+    match mdsrs with
+    | [] -> CGLeaf e
+    | mdsr::mdsrs ->
+        assert (mdsrs |> List.forall (fun r -> r = mdsr))
+        match mdsr with
+        | MDSRLeaf e -> CGLeaf e
+        | MDSRCon(c, es) -> 
+            assert (List.length es = List.length gs)
+            CGCon(c, gs)
+        | MDSRUnfold e -> 
+            assert (List.length gs = 1)
+            CGUnfold(List.head gs)
+        | MDSRCases(x, alts) ->
+            assert (List.length alts = List.length gs)
+            CGCases(x, List.zip (List.map fst alts) gs)
+        | MDSRLet(binds, _e) ->
+            assert (List.length binds + 1 = List.length gs)
+            let (g::gs) = gs
+            CGLet(List.zip (List.map fst binds) gs, g)
 
-let rec gset2graphs (gs: GraphSet) : seq<ConfGraph> =
+let rec gset2graphs (gs: GraphSet) : seq<MConf * ConfGraph> =
     match gs with
     | GSNone -> Seq.empty
-    | GSFold(n, ren) -> seq{ yield CGFold(n, ren) }
-    | GSBuild(mdsr, xss) ->
+    | GSFold(conf, n, ren) -> seq{ yield (conf, CGFold(n, ren)) }
+    | GSBuild(conf, xss) ->
         xss
         |> Seq.collect (fun xs -> xs |> Seq.map gset2graphs |> Seq.cartesian)
-        |> Seq.map (buildGraph mdsr)
+        |> Seq.map (fun cgs -> (conf, buildGraph (snd conf) cgs))
 
 type private HistEntryKind = HEGlobal | HELocal
 
-let rec private mrScpRec (defs: Defs) (nestLvl: int) (hist: list<HistEntryKind * int * Exp>) (e: Exp) : GraphSet =
+let rec private mrScpRec (defs: Defs) (nestLvl: int) (hist: list<HistEntryKind * int * Exp>) (conf: MConf) : GraphSet =
+    let (_, e) = conf
     let relevantHist hek hist =
         match hek with
         | HELocal -> hist |> List.takeWhile (fun (hek, _, _) -> hek = HELocal)
         | HEGlobal -> hist |> List.filter (fun (hek, _, _) -> hek = HEGlobal)
     match List.tryFindSome (fun (_, _, e1) -> renaming e1 e) hist with
-    | Some ((_, lvl, _), ren) -> GSFold(nestLvl - lvl, ren)
+    | Some ((_, lvl, _), ren) -> GSFold(conf, nestLvl - lvl, ren)
     | None ->
         let rs = multiDriveSteps defs e
         let hek = if List.tryFind (fun mdsr -> match mdsr with MDSRCases _ -> true | _ -> false) rs = None
@@ -53,6 +62,9 @@ let rec private mrScpRec (defs: Defs) (nestLvl: int) (hist: list<HistEntryKind *
         match List.tryFind (fun (_, _, e1) -> homeomorphicEmbedding e1 e) relHist with
         | Some _ -> GSNone
         | None ->
-            GSBuild()
+            let confss = rs |> List.map (fun mdsr -> mdsrSubExps mdsr |> List.map (fun e -> (mdsr, e)))
+            let newHist = (hek, nestLvl, e)::hist
+            GSBuild(conf, List.map (List.map (mrScpRec defs (nestLvl + 1) newHist)) confss)
 
-    
+let mrScp (defs: Defs) (e: Exp) : GraphSet =
+    mrScpRec defs 0 [] (MDSRUnfold e, e)
--- src/MRScpOptSize/MRScpOptSize/Program.fs
+++ src/MRScpOptSize/MRScpOptSize/Program.fs
@@ -4,7 +4,15 @@
 [<EntryPoint>]
 let main argv = 
     //let (paa, eaa) = Examples.appAppExample ()
+    (*
     let (paa, eaa) = Examples.boolEqSymExample ()
     printfn "%A" paa
     printfn "%A" eaa
+    *)
+    let gs = MRScp.mrScp (ExpParser.str2defs "f(x,y)=Pair(x,y);") (ExpParser.str2exp "f(A,B)")
+    printfn "%A" gs
+    let cgs = MRScp.gset2graphs gs
+    for (c, g) in cgs do
+        printfn "%A" c
+        printfn "%A" g
     0 // return an integer exit code
